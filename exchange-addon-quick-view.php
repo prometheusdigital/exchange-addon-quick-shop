69e7ca216c33d7ddc3e5bbf009feb1ab3a6d1b84
Lew Ayotte <lew@ithemes.com>
2015-11-05 10:28:11 -0600

    Tagging 0.0.7

--- exchange-addon-quick-view.php
+++ exchange-addon-quick-view.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Quick View Add-on
- * Version: 0.0.6
+ * Version: 0.0.7
  * Description: Adds the quick view feature to iThemes Exchange store products.
  * Plugin URI: http://ithemes.com/exchange/quick-view/
  * Author: iThemes
